154a261480b4b54b285da6a7d9109fafb81c40d2
brandonquinonez <brandonvquinonez23@gmail.com>
2026-07-17 23:41:09 -0700

    created AddItemModal.jsx in components for Refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,13 @@ import "./App.css";
 import {
   coordinates,
   apiKey,
-  weatherTypes,
   defaultClothingItems,
 } from "../../utils/constants.js";
 import { useModalClose } from "../../hooks/useModalClose.js";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
-import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import AddItemModal from "../AddItemModal/AddItemModal";
 import ItemModal from "../ItemModal/ItemModal";
 import { getWeather, filterWeatherData } from "../../utils/weatherApi.js";
 import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext.jsx";
@@ -21,9 +20,8 @@ function App() {
     type: "",
     temp: { F: "", city: "" },
   });
-  const [clothingItems, setClothingItems] = useState(defaultClothingItems);
+  const [clothingItems] = useState(defaultClothingItems);
   const [activeModal, setActiveModal] = useState("");
-  // If I want to see a previw of a modal it'll show up by adding preview inside the quotes above
   const [selectedCard, setSelectedCard] = useState({});
   const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState("F");
 
@@ -69,79 +67,11 @@ function App() {
           />
         </div>
         <Footer />
-        <ModalWithForm
-          title="New garment"
-          buttonText="Add garment"
-          name="add-garment"
+        <AddItemModal
           isOpen={activeModal === "add-garment"}
           onClose={closeActiveModal}
-        >
-          <label htmlFor="name" className="modal__label">
-            Name{" "}
-            <input
-              type="text"
-              className="modal__input"
-              id="name"
-              placeholder="Name"
-            />
-          </label>
-
-          <label htmlFor="imageUrl" className="modal__label">
-            Image URL{" "}
-            <input
-              type="url"
-              className="modal__input"
-              id="imageUrl"
-              placeholder="Image URL"
-            />
-          </label>
-
-          <fieldset className="modal__radio-buttons">
-            <legend className="modal__legend">Select the weather type:</legend>
-
-            <label
-              htmlFor="hot"
-              className="modal__label modal__label_type_radio"
-            >
-              <input
-                id="hot"
-                type="radio"
-                name="weather"
-                value={weatherTypes.hot}
-                className="modal__radio-input"
-              />
-              Hot
-            </label>
-
-            <label
-              htmlFor="warm"
-              className="modal__label modal__label_type_radio"
-            >
-              <input
-                id="warm"
-                type="radio"
-                name="weather"
-                value={weatherTypes.warm}
-                className="modal__radio-input"
-              />
-              Warm
-            </label>
-
-            <label
-              htmlFor="cold"
-              className="modal__label modal__label_type_radio"
-            >
-              <input
-                id="cold"
-                type="radio"
-                name="weather"
-                value={weatherTypes.cold}
-                className="modal__radio-input"
-              />
-              Cold
-            </label>
-          </fieldset>
-        </ModalWithForm>
+          handleSubmit={(event) => event.preventDefault()}
+        />
         <ItemModal
           isOpen={activeModal === "preview"}
           card={selectedCard}
