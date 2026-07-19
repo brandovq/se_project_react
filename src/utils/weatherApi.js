a28c4a137cae0f3b236c0f6891c4eea06add9662
brandonquinonez <brandonvquinonez23@gmail.com>
2026-07-19 12:48:59 -0700

    Exported handleServerResponse from api.js and imported it into weatherApi.js to remove duplicated res.ok checking

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,16 +1,11 @@
 import { weatherTypes } from "./constants.js";
+import { handleServerResponse } from "./api.js";
 
 export const getWeather = ({ latitude, longitude }, apiKey) => {
   return fetch(
     // its in farenheit because we're specifying &units=imperial in the API request below
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`,
-  ).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
+  ).then(handleServerResponse);
 };
 
 export const filterWeatherData = (data) => {
